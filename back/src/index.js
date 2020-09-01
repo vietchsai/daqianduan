02f1c1851080692105e147b33096a9dff6b2329d
vietchsai <vietchsai@gmail.com>
2020-09-01 11:49:38 +0800

    增加到路由

--- back/src/index.js
+++ back/src/index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa');
+const path = require('path');
 const KoaBody = require('koa-body');
 const json = require('koa-json');
 const cors = require('@koa/cors');
+const helmet = require('koa-helmet');
+const statics = require('koa-static');
 
 // 实例化
 const app = new Koa();
@@ -10,6 +13,8 @@ const router = require('./routes/routes');
 app
   .use(KoaBody())
   .use(cors())
+  .use(helmet())
+  .use(statics(path.join(__dirname, '../public'), 'static'))
   .use(json({pretty: false, param: 'pretty'}))
   .use(router());
 
